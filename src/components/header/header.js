d395c189ebbb27256da2fab1f4a0b9515932b797
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2022-08-29 13:15:06 -0300

    fixed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,14 @@
-import IconPerfil from "../../assets/images-header/icon.svg"
 import logo from '../../assets/images-header/spotify.png';
+import foto from '../../assets/images-header/foto.png';
+import {FiAlignJustify, FiXCircle} from "react-icons/fi"
+import React, {useState} from "react"
 import './header.scss';
 
 export default function Header(){
-    
-    
+    const [mode, setMode] = useState(false);
+    function toggleMode(){
+        setMode(!mode);
+    }
     return(
         <header>
             <div>
@@ -22,13 +26,41 @@ export default function Header(){
                         <a href="/">Baixar</a>
                     </li>
                     <li id='baixar'></li>
-                    <li>
-                        <div>
-                            <a href="/">Perfil</a>
-                        </div>
+                    <li onClick={toggleMode} id="perfil">
+                        <img src={foto} alt="perfil" id='icon'/>
+                        Perfil
+                        <FiAlignJustify style={{height: "120px", marginLeft: "10px"}}/>
+                    </li>
+                    <li className='no-exist'>
+                        { mode && (
+                            <ul className="container-popup">
+                                <li>
+                                    <a href='/' className='minWidth'>Linkedin</a>
+                                </li>
+                                <li>
+                                    <a href='/' className='minWidth'>github</a>
+                                </li>
+                                <li id='barra'>
+                                    
+                                </li>
+                                <li>
+                                    <a href="/">Baixar</a>
+                                </li>
+                                <li>
+                                    <a href="/">Suporte</a>
+                                </li>
+                                <li>
+                                    <a href="/">Premium</a>
+                                </li>
+                                <div id="divClose"onClick={toggleMode}>
+                                    <FiXCircle id="close"/>
+                                </div>
+                            </ul>
+                        )}
                     </li>
                 </ul>
             </div>
+                        
         </header>
     )
 }
